0149c40bc569d86ebe4aa71ebaf8e38fb966c7ba
Shiroshana Tissera <devwork.rocketbox@gmail.com>
2017-12-16 19:28:12 +0000

    first commit

---
+++ aws_py3/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/siyanetissera/development/githubRepo/aws_projects/aws_py3/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
