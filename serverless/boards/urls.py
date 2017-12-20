bd414b00db28c16fd18457d52c3f694ef6d1eb7e
Shiroshana Tissera <devwork.rocketbox@gmail.com>
2017-12-20 20:37:09 +0000

    commit to master

--- serverless/boards/urls.py
+++ serverless/boards/urls.py
@@ -1,8 +1,12 @@
-from django.conf.urls import include, url
+from django.conf.urls import url
 from django.contrib import admin
+
 from boards import views
-                    
+
 
 urlpatterns = [
-    url(r'^$', views.index, name = 'home'),
+    url(r'^$', views.index, name='home'),
+    url(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
+    url(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
+    url(r'^admin/', admin.site.urls),
 ]
